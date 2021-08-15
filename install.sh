ce1187a076abe18bf48dd21b99dadb92b298063a
Matthias Ferber <matthias@cantinaconsulting.com>
2021-08-14 20:56:02 -0400

    Fixed support for spaces in pathnames

--- install.sh
+++ install.sh
@@ -13,21 +13,21 @@ function main {
 function install {
     DOTFILES_DIR="$(resolve_script_dir)/"
     SYMLINK_LOCATION=$2
-    SYMLINK_TARGET="${DOTFILES_DIR}src/$1"
-    SYMLINK_LOCATION_DISPLAY=$(shorten_path $SYMLINK_LOCATION)
-    SYMLINK_TARGET_DISPLAY=$(shorten_path $SYMLINK_TARGET)
+    SYMLINK_TARGET=${DOTFILES_DIR}src/"$1"
+    SYMLINK_LOCATION_DISPLAY=$(shorten_path "$SYMLINK_LOCATION")
+    SYMLINK_TARGET_DISPLAY=$(shorten_path "$SYMLINK_TARGET")
     red="\033[0;91m"
     green="\033[0;92m"
     reset="\033[0m"
 
-    if [ -L $SYMLINK_LOCATION ]; then
-        ACTUAL_SYMLINK_TARGET=$(readlink $SYMLINK_LOCATION)
+    if [ -L "$SYMLINK_LOCATION" ]; then
+        ACTUAL_SYMLINK_TARGET=$(readlink "$SYMLINK_LOCATION")
         if [ "$ACTUAL_SYMLINK_TARGET" = "${SYMLINK_TARGET}" ]; then
             echo -e "${green}✓ $SYMLINK_LOCATION_DISPLAY:${reset} verified symlink -> $SYMLINK_TARGET_DISPLAY"
         else
             echo -e "${red}𐄂 $SYMLINK_LOCATION_DISPLAY:${reset} conflicting symlink exists -> $(shorten_path $ACTUAL_SYMLINK_TARGET)"
         fi
-    elif [ -e $SYMLINK_LOCATION ]; then
+    elif [ -e "$SYMLINK_LOCATION" ]; then
         echo -e "${red}𐄂 $SYMLINK_LOCATION_DISPLAY:${reset} file exists at this location"
     else
         ln -s "$SYMLINK_TARGET" "$SYMLINK_LOCATION"

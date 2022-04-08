f4b57c468f8fe65454446f5c07aa082b64954863
Matthias Ferber <matthias@cantinaconsulting.com>
2022-04-08 13:45:55 -0400

    git prompt: use python3

--- src/zsh/git-prompt/zshrc.sh
+++ src/zsh/git-prompt/zshrc.sh
@@ -2,7 +2,8 @@
 # CUSTOMIZATIONS - MAF
 #
 #   * Added stash count checking
-#   * Customized appe arances
+#   * Customized appearances
+#   * Upgraded to Python3
 #
 
 # To install source this file from your .zshrc file
@@ -52,7 +53,7 @@ function update_current_git_vars() {
 
     if [[ "$GIT_PROMPT_EXECUTABLE" == "python" ]]; then
         local gitstatus="$__GIT_PROMPT_DIR/gitstatus.py"
-        _GIT_STATUS=`python ${gitstatus} 2>/dev/null`
+        _GIT_STATUS=`python3 ${gitstatus} 2>/dev/null`
     fi
 
     if [[ "$GIT_PROMPT_EXECUTABLE" == "haskell" ]]; then

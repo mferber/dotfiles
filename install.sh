6ece1ad37b9f518bc2ea3b88b4854959809814fc
Matthias Ferber <matthias.ferber@gmail.com>
2022-05-03 11:42:36 -0400

    nvUltra: disabled pending a sandbox workaround
    nvUltra is sandboxed and can't access symlinked files outside of its own
    private directory. Disabled its configs from installation until I can come
    up with a tolerable workaround (probably involving copying the files
    instead of symlinking them, with some diff checks and user confirmation to
    avoid accidents)

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ function main {
     install vscode_settings.json ~/Library/'Application Support'/Code/User/settings.json
     install vscode_settings.json ~/Library/'Application Support'/VSCodium/User/settings.json
     install ideavimrc ~/.ideavimrc
-    install 'nvUltra_Matthias Customized.css' ~/'Library/Group Containers/group.com.multimarkdown/Library/Application Support/MultiMarkdown Software/CSS/Matthias Customized.css'
+    # install 'nvUltra_Matthias Customized.css' ~/'Library/Group Containers/group.com.multimarkdown/Library/Application Support/MultiMarkdown Software/CSS/Matthias Customized.css'
     install vlcrc ~/Library/Preferences/org.videolan.vlc/vlcrc
     install httpie_config.json ~/.config/httpie/config.json
     echo
